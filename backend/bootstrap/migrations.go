0fd53a9f193b0e62384f3aefeb87e5a225a78ac2
polibee <1244762327@qq.com>
2026-09-21 10:27:09 +0800

    fix: register user status migration

--- backend/bootstrap/migrations.go
+++ backend/bootstrap/migrations.go
@@ -12,5 +12,6 @@ func Migrations() []schema.Migration {
 		&migrations.M20260920000001CreateUsersTable{},
 		&migrations.M20260920000002CreateRBACTables{},
 		&migrations.M20260920000003AddRBACTimestamps{},
+		&migrations.M20260921000001ReplaceUserActiveWithStatus{},
 	}
 }
